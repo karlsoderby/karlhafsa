926e069b9f3de1ea35f12d0fa8b8c8a57a309057
karlsoderby <35461661+karlsoderby@users.noreply.github.com>
2018-02-22 23:38:36 +0100

    Update script.js

--- karlsketch/public/script.js
+++ karlsketch/public/script.js
@@ -1,9 +1,6 @@
 var socket = null;
-var postresult = "Coffee is brewing...";
-var postresult1 ="Coffee pot off.";
-var postresult2 ="Lights turned on.";
-var postresult3 ="Lights turned off.";
-var postresult4 ="On for 30 sec.";
+var postresult = "LED is dimming..";
+
 
 
 
@@ -32,29 +29,4 @@ document.getElementById("results").innerHTML= postresult;
 document.getElementById("results").style.background = "green";
 }
 
-function displayDiv1() {
-	document.getElementById("results").innerHTML= postresult1;
-	document.getElementById("results").style.background = "red";
-	}
 
-	function displayDiv2() {
-		document.getElementById("results1").innerHTML= postresult2;
-		document.getElementById("results1").style.background = "green";
-		}
-
-
-		function displayDiv3() {
-			document.getElementById("results1").innerHTML= postresult3;
-			document.getElementById("results1").style.background = "red";
-			}
-
-			function displayDiv4() {
-				document.getElementById("results2").innerHTML= postresult4;
-				document.getElementById("results2").style.background = "green";
-				}
-		
-		
-				function displayDiv5() {
-					document.getElementById("results2").innerHTML= postresult5;
-					document.getElementById("results2").style.background = "red";
-					}
